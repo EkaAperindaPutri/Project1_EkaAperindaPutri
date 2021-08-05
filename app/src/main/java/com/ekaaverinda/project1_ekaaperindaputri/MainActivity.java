5d41902a17ab8561e154796813dd79c620a5ec13
EkaAperindaPutri <80092497+EkaAperindaPutri@users.noreply.github.com>
2021-08-05 18:00:13 +0700

    Initial Commit

---
+++ app/src/main/java/com/ekaaverinda/project1_ekaaperindaputri/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ekaaverinda.project1_ekaaperindaputri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
