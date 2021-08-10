e12953c545f750293e8d2bec3ce4d667bd7ad4f9
EkaAperindaPutri <80092497+EkaAperindaPutri@users.noreply.github.com>
2021-08-10 16:40:37 +0700

    Deklarasi variabel dan buat method tampil

--- app/src/main/java/com/ekaaverinda/project1_ekaaperindaputri/MainActivity.java
+++ app/src/main/java/com/ekaaverinda/project1_ekaaperindaputri/MainActivity.java
@@ -3,12 +3,28 @@ package com.ekaaverinda.project1_ekaaperindaputri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Deklarasikan Variabel
+    EditText TextNama;
+    TextView Hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Panggil Variabel berdasarkan id
+        TextNama=(EditText)findViewById(R.id.name);
+        Hasil=(TextView)findViewById(R.id.btn_name);
+    }
+
+    //buat method tampil nama sesuai dengan property Onclick pada button
+    public void TampilNama(View v) {
+        Hasil.setText("Nama Anda: "+TextNama.getText());
     }
 }
